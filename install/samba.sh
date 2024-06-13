20ddcf768704854c50f70b8bd3e68f51b82c677c
LJT1109 <ljt.tw@outlook.com>
2024-06-14 01:11:09 +0800

    20240614

--- install/samba.sh
+++ install/samba.sh
@@ -1,11 +1,18 @@
 sudo mkdir /mnt/storage0
-sudo mount /dev/sda2 /mnt/storage0/
+sudo mount /dev/sdb /mnt/storage0/
 
 sudo docker run -d -p 139:139 -p 445:445 --hostname $HOSTNAME \
     --name samba --restart unless-stopped \
     -v ljt:/mnt/storage0 elswork/samba \
     -u "$(id -u):$(id -g):$(id -un):$(id -gn):Aa27378745" \
     -s "ljt:/mnt/storage0:rw:$(id -un)"
+
+sudo docker run -d -p 139:139 -p 445:445 --hostname $HOSTNAME \
+    --name samba --restart unless-stopped \
+    -v /mnt/storage0:/mnt/storage0 elswork/samba \
+    -u "$(id -u):$(id -g):$(id -un):$(id -gn):Aa27378745" \
+    -s "ljt:/mnt/storage0:rw:$(id -un)"
+
 
 sudo docker stop samba
 sudo docker rm samba
